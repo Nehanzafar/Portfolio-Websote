2ae6bbfdce9f8388de372e4d13c8ce7b51dfb8e8
Nehanzafar <142224878+Nehanzafar@users.noreply.github.com>
2025-06-04 15:43:21 +0500

    Fix homepage URL in package.json and update routing in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,21 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles/index.css";
+import { createBrowserRouter, RouterProvider } from "react-router";
+
+const routes = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <h1>Home Page</h1>,
+      children: [],
+    },
+  ],
+  { basename: "/Portfolio-Website" }
+);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <div>
-      <h1 className="text-2xl text-accent-yellow">Hello World</h1>
-    </div>
+    <RouterProvider router={routes} />
   </StrictMode>
 );
